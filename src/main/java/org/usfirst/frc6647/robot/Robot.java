dbfc69d0f4ff0b34d6a01941f74a5645a8fffc00
pacoito123 <pacoito@protonmail.com>
2020-02-23 06:06:01 -0600

    Added some pre-mapped controllers. [OI] - Added a bunch of controllers to Profiles.json.

--- src/main/java/org/usfirst/frc6647/robot/Robot.java
+++ src/main/java/org/usfirst/frc6647/robot/Robot.java
@@ -8,6 +8,9 @@
 package org.usfirst.frc6647.robot;
 
 import org.usfirst.lib6647.loops.LooperRobot;
+import org.usfirst.lib6647.oi.JController;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Robot extends LooperRobot {
 	/** Static {@link Robot} instance. */
@@ -32,7 +35,32 @@ public class Robot extends LooperRobot {
 		if (instance == null) // Might not be necessary, but just in case.
 			instance = this;
 
-		// initJoysticks();
+		initJoysticks();
 		// registerSubsystems(Chassis::new, Intake::new, ...);
+	}
+
+	/**
+	 * Run any {@link JController} initialization here.
+	 */
+	private void initJoysticks() {
+		// Create JController object.
+		var driver1 = new JController(0);
+
+		System.out.printf("Found: '%s'!\n", driver1.getName());
+
+		if (driver1.getName().equals("Wireless Controller")) {
+			driver1.setXY(Hand.kLeft, 0, 1);
+			driver1.setXY(Hand.kRight, 2, 5);
+		} else if (driver1.getName().equals("Generic   USB  Joystick")) {
+			driver1.setXY(Hand.kLeft, 0, 1);
+			driver1.setXY(Hand.kRight, 2, 4);
+		} else if (driver1.getName().toLowerCase().contains("xbox")
+				|| driver1.getName().equals("Controller (Gamepad F310)")) {
+			driver1.setXY(Hand.kLeft, 0, 1);
+			driver1.setXY(Hand.kLeft, 4, 5);
+		}
+
+		// Register each instantiated JController object in the joysticks HashMap.
+		registerJoystick(driver1, "driver1");
 	}
 }
